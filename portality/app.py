69483e394a19eeefc25c9bd6e94f411d2ba6c649
Nevelina <nevelina.aleksandrova@gmail.com>
2014-06-30 12:20:12 +0100

    Images can be seen fromt heir unique urls only by authenticated users

--- portality/app.py
+++ portality/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, abort, render_template, redirect, make_response, jsonify, send_file, send_from_directory
+from flask import Flask, request, abort, render_template, redirect, make_response, jsonify, send_file, \
+    send_from_directory
 from flask.views import View
 from flask.ext.login import login_user, current_user
 
@@ -11,10 +12,12 @@ from portality.view.account import blueprint as account
 from portality.view.query import blueprint as query
 from portality.view.advert import blueprint as advert
 
+
 @login_manager.user_loader
 def load_account_for_login_manager(userid):
     out = models.Account.pull(userid)
     return out
+
 
 @app.before_request
 def standard_authentication():
@@ -38,6 +41,7 @@ app.register_blueprint(account, url_prefix='/account')
 app.register_blueprint(query, url_prefix='/user_query')
 app.register_blueprint(advert, url_prefix='/advert')
 
+
 @app.route("/")
 def root():
     if current_user.is_authenticated() and current_user.has_role("user"):
@@ -45,36 +49,41 @@ def root():
     else:
         return render_template("welcome.html")
 
+
 @app.route('/autocomplete/<doc_type>/<field_name>', methods=["GET", "POST"])
 def autocomplete(doc_type, field_name):
-    prefix = request.args.get('q','').lower()
+    prefix = request.args.get('q', '').lower()
 
     if not prefix:
-        return jsonify({'suggestions':[{"id":"", "text": "No results found"}]})  # select2 does not understand 400, which is the correct code here...
+        return jsonify({'suggestions': [{"id": "",
+                                         "text": "No results found"}]})  # select2 does not understand 400, which is the correct code here...
 
-    #Because AdBock blocks anything with the word advert in it, the doc_type the autocomplete submits had to be renamed.
+    # Because AdBock blocks anything with the word advert in it, the doc_type the autocomplete submits had to be renamed.
     if doc_type == 'adsubmit':
         doc_type = 'advert'
     m = models.lookup_model(doc_type)
     if not m:
-        return jsonify({'suggestions':[{"id":"", "text": "No results found"}]})  # select2 does not understand 404, which is the correct code here...
+        return jsonify({'suggestions': [{"id": "",
+                                         "text": "No results found"}]})  # select2 does not understand 404, which is the correct code here...
 
     size = request.args.get('size', 5)
     return jsonify({'suggestions': m.autocomplete(field_name, prefix, size=size)})
     # you shouldn't return lists top-level in a JSON response:
     # http://flask.pocoo.org/docs/security/#json-security
 
+
 @app.route("/user_uploads/<image_name>")
 def serve_user_uploads(image_name):
-    # if current_user.is_authenticated() and current_user.has_role("user"):
-    #
-    return send_from_directory(app.config['IMAGES_FOLDER'], image_name)
-    # else:
-    #     abort(401)
+    if current_user.is_authenticated() and current_user.has_role("user"):
+        return send_from_directory(app.config['IMAGES_FOLDER'], image_name)
+    else:
+        abort(401)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=app.config['PORT'])
