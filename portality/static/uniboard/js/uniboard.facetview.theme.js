f75d6a9a0459d7c2511b46a73beab57ac46ae1e3
Richard Jones <richard@cottagelabs.com>
2014-08-01 10:03:55 +0100

    modify share-save button on public search

--- portality/static/uniboard/js/uniboard.facetview.theme.js
+++ portality/static/uniboard/js/uniboard.facetview.theme.js
@@ -66,6 +66,87 @@ jQuery(document).ready(function($) {
         return result;
     }
 
+
+    function uniSearchOptions(options) {
+        /*****************************************
+         * overrides must provide the following classes and ids
+         *
+         * class: facetview_startagain - reset the search parameters
+         * class: facetview_pagesize - size of each result page
+         * class: facetview_order - ordering direction of results
+         * class: facetview_orderby - list of fields which can be ordered by
+         * class: facetview_searchfield - list of fields which can be searched on
+         * class: facetview_freetext - input field for freetext search
+         *
+         * should (not must) respect the following configs
+         *
+         * options.search_sortby - list of sort fields and directions
+         * options.searchbox_fieldselect - list of fields search can be focussed on
+         * options.sharesave_link - whether to provide a copy of a link which can be saved
+         */
+
+        // initial button group of search controls
+        var thefacetview = '<div class="btn-group" style="display:inline-block; margin-right:5px;"> \
+            <a class="btn btn-small facetview_startagain" title="clear all search settings and start again" href=""><i class="icon-remove"></i></a> \
+            <a class="btn btn-small facetview_pagesize" title="change result set size" href="#"></a>';
+
+        if (options.search_sortby.length > 0) {
+            thefacetview += '<a class="btn btn-small facetview_order" title="current order descending. Click to change to ascending" \
+                href="desc"><i class="icon-arrow-down"></i></a>';
+        }
+        thefacetview += '</div>';
+
+        // selection for search ordering
+        if (options.search_sortby.length > 0) {
+            thefacetview += '<select class="facetview_orderby" style="border-radius:5px; \
+                -moz-border-radius:5px; -webkit-border-radius:5px; width:100px; background:#eee; margin:0 5px 21px 0;"> \
+                <option value="">order by ... relevance</option>';
+
+            for (var each = 0; each < options.search_sortby.length; each++) {
+                var obj = options.search_sortby[each];
+                var sortoption = '';
+                if ($.type(obj['field']) == 'array') {
+                    sortoption = sortoption + '[';
+                    sortoption = sortoption + "'" + obj['field'].join("','") + "'";
+                    sortoption = sortoption + ']';
+                } else {
+                    sortoption = obj['field'];
+                }
+                thefacetview += '<option value="' + sortoption + '">' + obj['display'] + '</option>';
+            };
+            thefacetview += '</select>';
+        }
+
+        // select box for fields to search on
+        if ( options.searchbox_fieldselect.length > 0 ) {
+            thefacetview += '<select class="facetview_searchfield" style="border-radius:5px 0px 0px 5px; \
+                -moz-border-radius:5px 0px 0px 5px; -webkit-border-radius:5px 0px 0px 5px; width:100px; margin:0 -2px 21px 0; background:#ecf4ff;">';
+            thefacetview += '<option value="">search all</option>';
+
+            for (var each = 0; each < options.searchbox_fieldselect.length; each++) {
+                var obj = options.searchbox_fieldselect[each];
+                thefacetview += '<option value="' + obj['field'] + '">' + obj['display'] + '</option>';
+            };
+            thefacetview += '</select>';
+        };
+
+        // text search box
+        thefacetview += '<input type="text" class="facetview_freetext span4" style="display:inline-block; margin:0 0 21px 0; background:#ecf4ff;" name="q" \
+            value="" placeholder="search term" />';
+
+        // share and save link
+        if (options.sharesave_link) {
+            thefacetview += '<a class="btn btn-info facetview_sharesave pull-right" title="share a link to this search" style="margin:0 0 21px 5px;" href="">share <i class="icon-white icon-share-alt"></i></a>';
+            thefacetview += '<div class="facetview_sharesavebox alert alert-info" style="display:none;"> \
+                <button type="button" class="facetview_sharesave close">×</button> \
+                <p>Share or save this search:</p> \
+                <textarea class="facetview_sharesaveurl" style="width:100%;height:100px;">' + shareableUrl(options) + '</textarea> \
+                </div>';
+        }
+        return thefacetview
+    }
+
+
     var facets = [
         {
             'field': 'price',
@@ -128,7 +209,7 @@ jQuery(document).ready(function($) {
             {'display':'Publisher','field':'publisher'}
         ],
         render_result_record : discoveryRecordView,
-        
+        render_search_options : uniSearchOptions
     });
     
 });
