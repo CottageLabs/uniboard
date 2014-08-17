448777e8b08c5719b0698da5583c9886649e0370
Nevelina <nevelina.aleksandrova@gmail.com>
2014-08-17 16:31:44 +0100

    Adexpire test now cleans up after itself and is better structured

--- portality/test_adexpire.py
+++ portality/test_adexpire.py
@@ -1,8 +1,22 @@
 from portality.models import Advert
+from portality.scripts.adexpire import expire_email
+from time import sleep
+
+'''
+Expected output:
+
+advert 200
+advert_expired@cottagelabs.com
+expires_soon@cottagelabs.com
+still_okay@cottagelabs.com
+expires_soon@cottagelabs.com
+advert_expired@cottagelabs.com delete
+
+'''
 
 def create_expired():
     advert = Advert()
-    advert.set_owner('test@cottagelabs.com')
+    advert.set_owner('advert_expired@cottagelabs.com')
     advert.set_title('Hippy')
     advert.set_authors('Pony')
     advert.set_price(50)
@@ -13,7 +27,7 @@ def create_expired():
 
 def create_soon_expiring():
     advert = Advert()
-    advert.set_owner('test@cottagelabs.com')
+    advert.set_owner('expires_soon@cottagelabs.com')
     advert.set_title('Glug')
     advert.set_authors('Mug')
     advert.set_price(50)
@@ -24,15 +38,25 @@ def create_soon_expiring():
 
 def create_still_okay():
     advert = Advert()
-    advert.set_owner('test@cottagelabs.com')
+    advert.set_owner('still_okay@cottagelabs.com')
     advert.set_title('Foot')
     advert.set_authors('Feet')
     advert.set_price(50)
-    advert.expires_in(220)
+    advert.set_expires('2014-08-28T00:00:00Z')
     advert.save()
     print advert.owner
     return advert
 
-create_expired()
-create_soon_expiring()
-create_still_okay()
+
+expired = create_expired()
+soon_expiring = create_soon_expiring()
+still_okay = create_still_okay()
+sleep(2)
+
+expire_email(testing=True)
+
+expired.delete()
+soon_expiring.delete()
+still_okay.delete()
+
+
